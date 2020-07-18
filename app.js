cfcdfb6f3771c90d92107054408b1fc1a16bcd7d
do2bfree <alexey.tivonenko@gmail.com>
2020-07-18 23:19:04 +0300

    Updating slides values

--- app.js
+++ app.js
@@ -40,6 +40,7 @@ function randomColors() {
 
 		colorizeSliders(color, hue, brightness, saturation);
 	});
+	resetInputs();
 }
 
 function checkTextContrast(color, text) {
@@ -99,6 +100,27 @@ function updateText(index) {
 	for (let icon of icons) {
 		checkTextContrast(color, icon);
 	}
+}
+
+function resetInputs() {
+	const sliders = document.querySelectorAll(".sliders input");
+	sliders.forEach((slider) => {
+		if (slider.name === "hue") {
+			const hueColor = initialColors[slider.getAttribute("data-hue")];
+			const hueValue = chroma(hueColor).hsl()[0];
+			slider.value = Math.floor(hueValue);
+		}
+		if (slider.name === "brightness") {
+			const brightColor = initialColors[slider.getAttribute("data-bright")];
+			const brightValue = chroma(brightColor).hsl()[2];
+			slider.value = Math.floor(brightValue * 100) / 100;
+		}
+		if (slider.name === "saturation") {
+			const satColor = initialColors[slider.getAttribute("data-sat")];
+			const satValue = chroma(satColor).hsl()[1];
+			slider.value = Math.floor(satValue * 100) / 100;
+		}
+	});
 }
 
 randomColors();
